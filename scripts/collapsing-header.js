161cdf08fe15512b68f511b5d74115d6fd0cb8b7
fschnizzle <fschneider@student.unimelb.edu.au>
2021-05-29 12:27:08 +1000

    BROKEN
    Broken here

--- scripts/collapsing-header.js
+++ scripts/collapsing-header.js
@@ -1,11 +1,22 @@
 $(window).scroll(function() {
     if ($(this).scrollTop() > 1){  
+        // Changes
         $('header').addClass("fixed");
         $('.fa-shopping-cart').removeClass("bubble");
+        $('.header-top-middle-container .header-search').show();
+        $('.header-top-middle-container .header-logo-main').hide();
+        
     }
     else{
+        // Reverse Changes
         $('header').removeClass("fixed");
         $('.fa-shopping-cart').addClass("bubble");
+        $('.header-top-middle-container .header-search').hide();
+        $('.header-top-middle-container .header-logo-main').show();
 
     }
 });
+
+
+
+// Remove logo main, replace with search bar
